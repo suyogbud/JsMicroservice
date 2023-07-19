e40ff721b16e36f904e5f8dee1b840e434255474
Suyog Singh Budhathoki <suyogbc10@gmail.com>
2023-07-19 17:53:10 -0400

    Implemented a simple Restful APi

---
+++ server/server.js
@@ -0,0 +1,63 @@
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+
+let books = [];
+
+app.get('/books', (req, res) => {
+    res.json(books);
+})
+
+app.post('/books', (req, res) => {
+    const book = req.body;
+    books.push(book);
+    res.status(201).send();
+});
+
+app.put('books/:id', (req, res) => {
+    const id = req.params.id;
+    const book = req.body;
+
+    let index = books.findIndex((book) => book.id === id);
+    if (index !== -1) {
+        books[index] = book;
+        res.send();
+    } else {
+        res.status(404).send("Book not found");
+    }
+});
+
+
+app.delete('/books/:id', (req, res) => {
+    const id = req.params.id;
+
+
+    let index = books.findIndex((book) => book.id === id);
+    if (index !== -1) {
+        books.splice(index, 1);
+        res.send();
+    } else {
+        res.status(404).send();
+    }
+});
+
+
+const cors = require('cors');
+app.use(cors());
+
+const axios = require('axios');
+
+app.get('/posts', (req, res) => {
+    axios
+        .get('https://jsonplaceholder.typicode.com/posts')
+        .then((response) => {
+            res.json(response.data);
+        })
+        .catch((error) => {
+            res.status(500).send();
+        });
+});
+
+
+app.listen(3001, () => console.log('Server running on port 3001'));
