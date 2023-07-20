9edce15d746050ae47cfd8781bf17b2293763ec3
Suyog Singh Budhathoki <suyogbc10@gmail.com>
2023-07-20 02:46:55 -0400

    Refactored code for the rest api

--- server/server.js
+++ server/server.js
@@ -1,63 +1,57 @@
-const express = require('express');
+const express = require("express");
 const app = express();
+const axios = require("axios");
+const cors = require("cors");
+app.use(cors());
 
 app.use(express.json());
 
 let books = [];
 
-app.get('/books', (req, res) => {
-    res.json(books);
-})
-
-app.post('/books', (req, res) => {
-    const book = req.body;
-    books.push(book);
-    res.status(201).send();
+app.get("/books", (req, res) => {
+  res.json(books);
 });
 
-app.put('books/:id', (req, res) => {
-    const id = req.params.id;
-    const book = req.body;
-
-    let index = books.findIndex((book) => book.id === id);
-    if (index !== -1) {
-        books[index] = book;
-        res.send();
-    } else {
-        res.status(404).send("Book not found");
-    }
+app.post("/books", (req, res) => {
+  const book = req.body;
+  books.push(book);
+  res.status(201).send();
 });
 
+app.put("books/:id", (req, res) => {
+  const id = req.params.id;
+  const book = req.body;
 
-app.delete('/books/:id', (req, res) => {
-    const id = req.params.id;
-
-
-    let index = books.findIndex((book) => book.id === id);
-    if (index !== -1) {
-        books.splice(index, 1);
-        res.send();
-    } else {
-        res.status(404).send();
-    }
+  let index = books.findIndex((book) => book.id === id);
+  if (index !== -1) {
+    books[index] = book;
+    res.send();
+  } else {
+    res.status(404).send("Book not found");
+  }
 });
 
+app.delete("/books/:id", (req, res) => {
+  const id = req.params.id;
 
-const cors = require('cors');
-app.use(cors());
-
-const axios = require('axios');
-
-app.get('/posts', (req, res) => {
-    axios
-        .get('https://jsonplaceholder.typicode.com/posts')
-        .then((response) => {
-            res.json(response.data);
-        })
-        .catch((error) => {
-            res.status(500).send();
-        });
+  let index = books.findIndex((book) => book.id === id);
+  if (index !== -1) {
+    books.splice(index, 1);
+    res.send();
+  } else {
+    res.status(404).send();
+  }
 });
 
+app.get("/posts", (req, res) => {
+  axios
+    .get("https://jsonplaceholder.typicode.com/posts")
+    .then((response) => {
+      res.json(response.data);
+    })
+    .catch((error) => {
+      res.status(500).send();
+    });
+});
 
-app.listen(3001, () => console.log('Server running on port 3001'));
+app.listen(3001, () => console.log("Server running on port 3001"));
