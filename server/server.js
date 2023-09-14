7f730fc24dc1fe3c79bfeac5a20d5f30f3990f46
Suyog Singh Budhathoki <suyogbc10@gmail.com>
2023-09-14 00:41:53 -0400

    added db to server

--- server/server.js
+++ server/server.js
@@ -2,44 +2,73 @@ const express = require("express");
 const app = express();
 const axios = require("axios");
 const cors = require("cors");
+const { MongoClient } = require("mongodb");
 app.use(cors());
-
 app.use(express.json());
 
-let books = [];
+const mongoURI = "mongodb://127.0.0.1:27017";
+const dbName = "bookstore"; // Replace "bookstore" with your database name
 
-app.get("/books", (req, res) => {
-  res.json(books);
-});
+let db;
 
-app.post("/books", (req, res) => {
-  const book = req.body;
-  books.push(book);
-  res.status(201).send();
-});
+MongoClient.connect(mongoURI, { useUnifiedTopology: true })
+  .then((client) => {
+    db = client.db(dbName);
+    console.log("Connected to MongoDB");
+  })
+  .catch((error) => console.error("Error connecting to MongoDB:", error));
 
-app.put("books/:id", (req, res) => {
-  const id = req.params.id;
-  const book = req.body;
-
-  let index = books.findIndex((book) => book.id === id);
-  if (index !== -1) {
-    books[index] = book;
-    res.send();
-  } else {
-    res.status(404).send("Book not found");
+app.get("/books", async (req, res) => {
+  try {
+    const books = await db.collection("books").find().toArray();
+    res.json(books);
+  } catch (error) {
+    res.status(500).send("Error fetching books from database");
   }
 });
 
-app.delete("/books/:id", (req, res) => {
+app.post("/books", async (req, res) => {
+  const book = req.body;
+  try {
+    await db.collection("books").insertOne(book);
+    res.status(201).send();
+  } catch (error) {
+    res.status(500).send("Error adding the book to the database");
+  }
+});
+
+app.put("/books/:id", async (req, res) => {
+  const id = req.params.id;
+  const book = req.body;
+
+  try {
+    const result = await db
+      .collection("books")
+      .updateOne({ id: id }, { $set: book });
+
+    if (result.modifiedCount === 1) {
+      res.send();
+    } else {
+      res.status(404).send("Book not found");
+    }
+  } catch (error) {
+    res.status(500).send("Error updating the book in the database");
+  }
+});
+
+app.delete("/books/:id", async (req, res) => {
   const id = req.params.id;
 
-  let index = books.findIndex((book) => book.id === id);
-  if (index !== -1) {
-    books.splice(index, 1);
-    res.send();
-  } else {
-    res.status(404).send();
+  try {
+    const result = await db.collection("books").deleteOne({ id: id });
+
+    if (result.deletedCount === 1) {
+      res.send();
+    } else {
+      res.status(404).send("Book not found");
+    }
+  } catch (error) {
+    res.status(500).send("Error deleting the book from the database");
   }
 });
 
